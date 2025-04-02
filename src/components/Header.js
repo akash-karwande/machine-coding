4906964b30e74e01fc5787e41d7950743ed91936
Akash Karwande <akash.karwande@oracle.com>
2025-04-02 11:13:31 +0530

    css changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,7 @@ const Header = () => {
     return (
         <div className="header">
             <div className="logo-container">
-                <img className="logo" src="https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg" />
-            </div>
-            <div className="nav-container">
-                <ul className="nav-list">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                    <li>Cart</li>
-                </ul>
+                <h1>Machine Coding Interview Questions</h1>
             </div>
         </div>
     )
